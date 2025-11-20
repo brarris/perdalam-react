7f1f8f3a2a0d0a171ab0cb0755b6061753504bb3
brarris <farisabrar62@gmail.com>
2025-11-20 07:26:42 +0700

    Revert "Reapply "React Lifecycle with useEffect - from Component Mounting to Component Update (trigger Next Article button)""
    This reverts commit 337b86376a40f4c8e9a8da935629d378135aeec0.

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,6 @@ function App() {
     setCounterClick(counterClick + 1);
   };
 
-  const previousArticle = () => {
-    setCounterClick(counterClick - 1);
-  };
-
   return (
     <main>
       <WebTitle
@@ -56,7 +52,6 @@ function App() {
       />
       <Article title="Artikel Pertama" description="ini adalah deskripsi 1" />
       <Article title={article.title} description={article.body} />
-      <button onClick={previousArticle}>Previous Article</button>
       {counterClick}
       <button onClick={nextArticle}>Next Article</button>
     </main>
