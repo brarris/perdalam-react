d09d082758c15c880e7288e1bd2221e8645b81a3
brarris <farisabrar62@gmail.com>
2025-11-20 10:51:51 +0700

    Conditional Rendering - Ternary Operator Method (part 2)

--- src/App.jsx
+++ src/App.jsx
@@ -65,7 +65,7 @@ function App() {
       />
       <Article title="Artikel Pertama" description="ini adalah deskripsi 1" />
       <Article title={article.title} description={article.body} />
-      {counterClick > 4 ? "kondisi sesuai" : "kondisi tidak sesuai"}
+      {counterClick > 4 ? <LimitArticle /> : "kondisi tidak sesuai"}
       <button onClick={previousArticle}>Previous Article</button>
       {counterClick}
       <button onClick={nextArticle}>Next Article</button>
